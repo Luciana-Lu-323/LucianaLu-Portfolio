d1664d28f2cc658cc44a293924e4370df5869b63
Luciana-Lu-323 <lunasci323@gmail.com>
2025-10-05 00:09:21 +0800

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,11 @@
+// Smooth scroll for in-page links
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener('click', e=>{
+    const id = a.getAttribute('href');
+    if(id && id.length>1){
+      e.preventDefault();
+      document.querySelector(id)?.scrollIntoView({behavior:'smooth'});
+    }
+  });
+});
 
